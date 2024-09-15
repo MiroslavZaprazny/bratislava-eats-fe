6f126d99e7d0a7a598d3e4b89357143d0e835c36
MiroslavZaprazny <miro.zaprazny8@gmail.com>
2024-09-15 13:46:20 +0200

    launch page

--- lib/main.dart
+++ lib/main.dart
@@ -11,26 +11,20 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: 'Bratislava Eats',
-      home: Home(
-        initialAnimation: true,
-      ),
+      home: Home(),
     );
   }
 }
 
 class Home extends StatelessWidget {
-  const Home({super.key, required this.initialAnimation});
+  const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: const BoxDecoration(color: Color(0xFFFDFAE2)),
-      child: Container(
-        decoration: const BoxDecoration(
-            image: DecorationImage(image: AssetImage('assets/big-logo.png'))),
+    return const Scaffold(
+      body: Center(
+        child: Text("home page"),
       ),
     );
   }
-
-  final bool initialAnimation;
 }
